184b68613caf4c3a4ed68ef34eea88b224c759eb
Joshua-Ai <68153332+Joshua-Ai@users.noreply.github.com>
2022-01-26 11:00:19 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    token:          "OTIwOTIxMDg3OTgwNzUyOTc2.YbrYmQ.-A1JviX5RDYkgi2aHVXbGGaOurQ", 
+    emojis: {
+        success:    "<a:gerkie_tick:861472122194493451>",
+        error:      "<a:Cross:859803342003109898>",
+        online:     ":green_circle:",
+        dnd:        ":red_circle:",
+        offline:    ":white_circle:",
+        idle:       ":orange_circle:",
+        loading:    "<a:load:866205585011245066>",
+        warn:       "<:warn:862306784861618207>"
+    },
+  
+    color:  "BLURPLE",
+    footer: "Invite me to manage your server invites",
+    addLogs:  "779341728695451678",
+    removeLogs:  "779341728695451678",
+    shardLogs:  "779341728695451678",
+    dashLogs: "779341728695451678",
+
+    secret:         "tmUb31e5hux1-scsb03qi6ZOXrQzlWlq",
+    baseURL:        "",
+    port:           61100,
+    pswd: "XXXXXXXXXXX",
+    failureURL: "https://discord.com",  
+    mongodb: "mongodb+srv://KryptoRex:mycomputer2005@cluster0.liutu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    prefix: "v",
+    owners: ["808717271915823166"]
+};
+
